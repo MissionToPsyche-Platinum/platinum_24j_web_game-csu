414414a799d01a4a46dee6a4505dda95a3a42149
Parth Patel <ppatel2861866@gmail.com>
2026-02-17 16:10:26 -0500

    This script defines what a "Card" actually is based on your documentation.

---
+++ Assets/Scripts/CardData.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewCard", menuName = "Psyche/Card")]
+public class CardData : ScriptableObject 
+{
+    public string cardName;
+    
+    public enum CardType { Resource, Instrument, Maneuver, Analysis, Crisis }
+    public CardType type;
+
+    [Header("Costs")]
+    public int powerCost;
+    public int budgetCost;
+    public int timeCost;
+
+    [Header("Effects")]
+    public string description;
+    public int dataValue; // Used for Instrument cards to track data collected
+    public Sprite cardArt;
+    
+    public enum Rarity { Common, Uncommon, Rare }
+    public Rarity rarity;
+}
